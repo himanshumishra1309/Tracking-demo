df68cd1db311927b6a0cecfba93881c84d2c8533
himanshu mishra <himanshu.mishra0913@gmail.com>
2024-10-09 08:26:32 +0530

    made changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,38 +1,57 @@
 const socket = io();
 
-if(navigator.geolocation){
-    navigator.geolocation.watchPosition((position)=>{
-        const {longitude, latitude} = position.coords;
-        socket.emit('send-location', {longitude, latitude});
-    }, (error)=>{
+// Function to center map and place marker
+const addMarker = (latitude, longitude, id) => {
+    if (!markers[id]) {
+        markers[id] = L.marker([latitude, longitude]).addTo(map);  // Create a new marker
+    } else {
+        markers[id].setLatLng([latitude, longitude]);  // Update existing marker position
+    }
+};
+
+// Check if browser supports geolocation
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition((position) => {
+        const { longitude, latitude } = position.coords;
+
+        // Send user's location to the server
+        socket.emit('send-location', { longitude, latitude });
+
+        // Center the map on the user's location the first time
+        map.setView([latitude, longitude], 15);  // Set zoom level to 15 for a close-up view
+
+        // Place or update marker on user's location
+        addMarker(latitude, longitude, 'self');  // 'self' used as a unique identifier for the current user
+
+    }, (error) => {
         console.log(error);
-    },{
+    }, {
         enableHighAccuracy: true,
         timeout: 5000,
         maximumAge: 0
     });
 }
 
-const map = L.map("map").setView([0, 0], 10);
+// Initialize Leaflet map
+const map = L.map("map").setView([0, 0], 2);  // Initial view set to [0,0] and zoom 2
 
+// Load and add OpenStreetMap tiles to the map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-const markers = {};
+const markers = {};  // Store markers for all users
 
-socket.on('receive-location', (data)=>{
-    const {id, longitude, latitude} = data;
-    map.setView([latitude, longitude], 10);
-    if(markers[id]){
-        markers[id].setLatLng([latitude, longitude]);
-    }
-    else{
-        markers[id] = L.marker([latitude, longitude]).addTo(map);
-    }
-})
+// Receive other users' locations from the server
+socket.on('receive-location', (data) => {
+    const { id, longitude, latitude } = data;
 
-socket.on("user-disconnected", (id)=>{
-    if(markers[id]){
-        map.removeLayer(markers[id]);
-        delete markers[id];
+    // Center the map and add or update marker for other users
+    addMarker(latitude, longitude, id);
+});
+
+// Remove marker when a user disconnects
+socket.on("user-disconnected", (id) => {
+    if (markers[id]) {
+        map.removeLayer(markers[id]);  // Remove the marker from the map
+        delete markers[id];  // Remove the marker from the markers object
     }
 });
